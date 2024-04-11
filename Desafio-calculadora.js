edea40e7385a5492be599f81a36774a23aa84cb6
Hunterdown1505 <133170617+Hunterdown1505@users.noreply.github.com>
2024-04-10 21:05:10 -0300

    Add files via upload

--- Desafio-calculadora.js
+++ Desafio-calculadora.js
@@ -5,15 +5,15 @@ function calcularNivel(vitorias, derrotas){
 
         if(vitorias <=10){
             nivel = "Ferro"
-        }else if(vitorias >=11 && vitorias<=20){
+        }else if(vitorias >=11 && derrotas<=20){
             nivel = "Bronze"
-        }else if(vitorias >= 21 && vitorias<= 50){
+        }else if(vitorias >= 21 && derrotas<= 50){
             nivel = "Prata"
-        }else if (vitorias >= 51 && vitorias<=80){
+        }else if (vitorias >= 51 && derrotas<=80){
             nivel = "Ouro" 
-        }else if (vitorias >=81 && vitorias<=90){
+        }else if (vitorias >=81 && derrotas<=90){
             nivel = "Diamante"
-        }else if (vitorias >=91 && vitorias<=100){
+        }else if (vitorias >=91 && derrotas<=100){
             nivel = "Lendario"
         }else{
             nivel = "Imortal"
